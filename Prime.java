4d38ef15fee25a7f626292b40aa81907c739a586
Balu-Palle <150263734+Balu-Palle@users.noreply.github.com>
2024-01-17 19:30:08 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -8,36 +8,17 @@ public static void main (String args[])
 int n;
 Scanner s =new Scanner(System.in);
 n=s.nextInt();
-for(int i=2;i<n;i++)
+for(int i=1;i<n;i++)
 {
 int count=0;
-for(int j=1;j<i;j++)
-{
-if ((i%j)==0)
-{
+for (int j=1;j<=i;j++){
+if(i%j==0){
 count++;
 }
-if (count==2)
-{
+}
+if (count==2){
 System.out.println(i);
 }
 }
 }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
